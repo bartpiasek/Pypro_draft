65c923bcaf4c8727850c2da0464116943b8b98a5
Bart Piasek <bartpiasek@icloud.com>
2020-10-20 19:40:30 +0200

    Py list as database

---
+++ journal-project/database.py
@@ -0,0 +1,14 @@
+entries = []
+
+def add_entry(entry_content, entry_date):
+    """
+    docstring
+    """
+    entries.append({"content": entry_content, "date": entry_date})
+
+
+def get_entries():
+    """
+    docstring
+    """
+    return entries
